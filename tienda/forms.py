0d93b1ab34277ba1a84f9a358653ff7aa1289fbe
Rubiano718 <camilo.rubiano@estudiantesunibague.edu.co>
2026-05-18 15:51:05 -0500

    fix product image upload section

--- tienda/forms.py
+++ tienda/forms.py
@@ -135,12 +135,7 @@ class ProductoForm(forms.ModelForm):
     )
 
     imagenes_extra = forms.FileField(
-    required=False,
-    widget=forms.FileInput(attrs={
-        "class": "form-control",
-        "multiple": True,
-        "accept": "image/png,image/jpeg,image/webp,image/gif"
-    })
+    required=False
 )
     class Meta:
         model = Producto
